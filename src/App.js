0dcfc08c179f1441305cd45d71481b104c2b513d
aitorgb <aitorguerrero9@gmail.com>
2020-09-30 13:07:39 +0200

    Feat: Completed

--- src/App.js
+++ src/App.js
@@ -7,38 +7,38 @@ import { Redirect, Route, Switch } from 'react-router-dom';
 import Axios from 'axios';
 
 class App extends React.Component {
-  state = {
-    countries : []
-  }
+	state = {
+		countries: []
+	};
 
+	componentDidMount() {
+		Axios.get(`https://countries.tech-savvy.tech/countries`).then((res) => {
+			this.setState({ countries: res.data });
+		});
+	}
 
-  componentDidMount() {
-    Axios.get(`https://countries.tech-savvy.tech/countries`)
-      .then(res => {
-        this.setState({ countries : res.data });
-      })
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Navbar />
-        <div className="container mt-5">
-          <div className="row">
-            <div className="col-5 list">
-              <CountriesList CountriesList={this.state.countries} />
-            </div>
-            <div className="col-7">
-              <Switch>
-                <Route exact path="/country/:country" component={CountryDetails} />
-                <Redirect to="/" />
-              </Switch>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<div className="container-fluid  bg-primary">
+					<Navbar />
+				</div>
+				<div className="container mt-5">
+					<div className="row">
+						<div className="col-5 list">
+							<CountriesList CountriesList={this.state.countries} />
+						</div>
+						<div className="col-7">
+							<Switch>
+								<Route exact path="/country/:country" component={CountryDetails} />
+								<Redirect to="/" />
+							</Switch>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 export default function Navbar() {
   return (
-    <nav className="navbar navbar-dark bg-primary">
-      <span className="navbar-brand mb-0 h1">WikiCountries</span>
-    </nav>
+    <div className="container">
+      <nav className="navbar navbar-dark bg-primary">
+        <span className="navbar-brand mb-0 h1">WikiCountries</span>
+      </nav>
+    </div>
   );
 }
